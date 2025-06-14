5c4a2cb5fef80838977d3027a5cfb4bf7b5171df
goutam kumar <qwertyeng100@gmail.com>
2025-06-14 09:47:17 +0530

    Day1 file for array

--- src/DAY1/Array01.java
+++ src/DAY1/Array01.java
@@ -1,5 +1,7 @@
 package DAY1;
 
+import java.util.Arrays;
+
 public class Array01 {
 	//print the array
 		public static void printArray(int arr[]) {
@@ -23,7 +25,6 @@ public class Array01 {
 
 			int sl=-1;
 			int l=arr[0];	
-			int max=Integer.MIN_VALUE;
 			for(int i=1;i<arr.length-1;i++) {
 				if( arr[i]>l) {
 					sl=l;
@@ -61,43 +62,30 @@ public class Array01 {
 			return ss;
 		}
 		
-		//two pointer approach
-		public static void removingDuplicate(int arr[]) {
-//			int arr[]= {1,3,3,3,4,5,5,5,5,7,8,8};
-			//it is following a two pointer approach where j is a pointer which is getting updated and i is the pointer for old array
-			int j=0;
-			int arr1[]=new int[5];
-			for(int i=1;i<arr.length;i++) {
-				if(arr[i]!=arr[j]) {
-					arr[++j]=arr[i];
-				}
-			}	
-			for(int i=0; i<j;i++) {
-				System.out.print(arr[i]+" ");
-			}
-			
-		}
 		
 		//reverse the array
 		public static void reverseArray(int arr[]) {
 			int n=arr.length;
-			int temp=0;
-			for(int i=0;i<n/2;i++) {
-				temp=arr[i];
-				arr[i]=arr[n-i-1];
-				arr[n-i-1]=temp;
+			int left=0, right=n-1;
+			while(left<right){
+				int temp=arr[left];
+				arr[left]=arr[right];
+				arr[right]=temp;
+				left++;
+				right--;
 			}
 			printArray(arr);
 		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,3,3,3,4,5,5,5,5,7,8,8};
+		int arr[]= {1,2,3,4,5};
 		System.out.println("Elements of the array");
+		Arrays.sort(arr);
 		printArray(arr);
+		reverseArray(arr);
 		
 //		removingDuplicate(arr);
 		
-		reverseArray(arr);
 //		System.out.println("1nd largest number: "+largestval1(arr));
 //		System.out.println("2nd largest number: "+largestval2(arr));
 //		System.out.println("1st smallest number: "+smallval1(arr));
