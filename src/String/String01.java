a58d9667a7beb2d5b7bbb71f98eee46ab894be9f
Goutam1404 <goutamkumar9093@gmail.com>
2025-06-17 18:10:23 +0530

    Anagram using hashmap

--- src/String/String01.java
+++ src/String/String01.java
@@ -1,24 +1,23 @@
 package String;
-
+import java.util.*;
 public class String01 {
 	
 	//** isPalindrome
 	//These program is for string with no space and no special characters
 	public static boolean isPalindrome(String str) {
 		int n=str.length();
+		//racecar
 		for(int i=0;i<n/2;i++) {
 			if (Character.toLowerCase(str.charAt(i)) != Character.toLowerCase(str.charAt(n-i-1))) {
                 return false;
             }
 		}
-		//racecar
 		return true;
 	}
 
 	//These program is for string with space and special characters
 	  public static boolean isPalindromeSpecialCharacters(String s) {
 	        int left = 0, right = s.length() - 1;
-
 	        while (left < right) {
 	            
 	            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
@@ -41,7 +40,7 @@ public class String01 {
 	    }
 	
 	  //Anangram is the analogy for strings i.e it checks all the alphabates in both strings are equal or nor  
-	  public boolean isAnagram(String s, String t) {
+	  public static boolean isAnagram(String s, String t) {
 	        s=s.toLowerCase();
 	        t=t.toLowerCase();
 	
@@ -62,12 +61,37 @@ public class String01 {
 	    return true;
 	    }
 	  
+	  //** Anagram using hashmap
+	  public static boolean isAnagramHash(String s,String t) {
+		  HashMap<Character,Integer> map=new HashMap<>();
+		  for(char ch:s.toCharArray()) {
+			  map.put(ch, map.getOrDefault(ch, 0)+1);
+		  }
+		  for(char ch:t.toCharArray()) {
+			  if(map.get(ch)!=null) {
+				  if(map.get(ch)==1) {
+					  map.remove(ch);
+				  }else {
+					  map.put(ch, map.get(ch)-1);
+				  }
+			  } else {
+				  return false;
+				}
+		  }
+		  
+		  return map.isEmpty();
+	  }
+	  
 	  public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str1="raceCar";
 		String str2="A man, a plan, a canal: Panama";
 		System.out.println(str1+" is a palindrome :"+isPalindrome(str1));
-		System.out.println(str2+" is a palindrome :"+isPalindromeSpecialCharacters(str2));
+		System.out.println(str2+" is a palind	rome :"+isPalindromeSpecialCharacters(str2));
+		String s1="car";
+		String s2="rac";
+		System.out.println(isAnagram(s1,s2));
+		System.out.println(isAnagramHash(s1,s2));
 	}
 
 }
