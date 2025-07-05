39c5424c8d2d0e61f268ee7fe3ecc5008a8e35b6
Goutam1404 <goutamkumar9093@gmail.com>
2025-07-05 11:02:49 +0530

    selection sort

--- src/Array/SortingAlgos.java
+++ src/Array/SortingAlgos.java
@@ -8,7 +8,7 @@ public class SortingAlgos {
 		System.out.println();
 	}
 	
-	//taking the largest element to the last in each iteration
+	//taking the largest element to the last in each iteration O(n*n)
 	public static void bubbleSort(int arr[]) {
 		for(int i=0;i<arr.length-1;i++) {
 			int swap=0;
@@ -28,7 +28,7 @@ public class SortingAlgos {
 		}
 	}
 	
-	//Putting the smallest in the front
+	//Putting the smallest in the front O(n*n)
 	public static void selectionSort(int arr[]) {
 		 for(int i=0;i<arr.length-2;i++) {
 			 int minPos=i;
@@ -47,13 +47,30 @@ public class SortingAlgos {
 		
 	}
 	
+	//Each element is checked with there previous element O(n*n)
+	public static void insertionSort(int arr[]) {
+		for(int i=1;i<arr.length;i++) {
+			int curr=arr[i];
+			int prev=i-1;
+			//finding out the correct position to insert
+			while(prev>=0 && arr[prev]>curr) {
+				arr[prev+1]=arr[prev];
+				prev--;
+			}
+			//insertion
+			arr[prev+1]=curr;
+		}
+		printArr(arr);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[] = new int[6];
 		arr = new int[]{5,2,6,4,1,3};
 //		bubbleSort(arr);
 //		printArr(arr);
-		selectionSort(arr);
+//		selectionSort(arr);
+		insertionSort(arr);
 	}
 
 }
